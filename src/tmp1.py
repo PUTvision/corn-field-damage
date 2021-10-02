1446470529c4b6ac61da1412e5dd25e57a69cfce
Przemyslaw Aszkowski <przemyslaw.aszkowski@gmail.com>
2021-10-02 11:18:26 +0200

    Precise drawing of area using EPSG only. And simpler than latlong

--- src/tmp1.py
+++ src/tmp1.py
@@ -13,19 +13,11 @@ base_data_dir_path = "/home/przemek/Downloads/corn2/kukurydza_5_ha/"
 tif_file_path = os.path.join(base_data_dir_path, 'fotomapa.tif')
 
 geo_tiff = GeoTiff(tif_file_path)
-print(f'fotomapa_size = {geo_tiff.tif_bBox_wgs_84}')
-(x_left, y_upper), (x_right, y_bottom) = geo_tiff.tif_bBox_wgs_84
+print(f'fotomapa_size = {geo_tiff.tif_bBox}')
+(x_left, y_upper), (x_right, y_bottom) = geo_tiff.tif_bBox
 
-from pyproj import Geod
-geod = Geod(ellps="WGS84")
-lons = [x_left, x_right]  # x
-lats = [(y_bottom + y_upper) / 2] * 2
-x_diff_meters = geod.line_lengths(lons, lats)[0]
-x_diff_meters
-
-lons = [x_left] * 2  # x (no need to average anything, always same distance on every longitude
-lats = [y_bottom, y_upper]
-y_diff_meters = geod.line_lengths(lons, lats)[0]
+x_diff_meters = x_right - x_left
+y_diff_meters = y_upper - y_bottom
 
 
 
@@ -48,19 +40,16 @@ damage_file_path = os.path.join(base_data_dir_path, 'szkody_placowe.gpkg')
 gp_damage = geopandas.read_file(damage_file_path)
 
 
-transformer = Transformer.from_crs("epsg:32633", "wgs84")
-
 area_polygon = gp_area.to_numpy()[0][0][0]
 x_epsg, y_epsg = area_polygon.exterior.coords.xy
-y_wgs, x_wgs = transformer.transform(x_epsg, y_epsg)
 
-pixels_per_x_wgs = (x_pixels - 1) / (x_right - x_left)
-pixels_per_y_wgs = (y_pixels - 1) / (y_upper - y_bottom)
+pixels_per_x_epsg = 1 / spacial_resolution_xy_meters[0]
+pixels_per_y_epsg = 1 / spacial_resolution_xy_meters[1]
 
-yx_px = np.zeros((len(x_wgs), 2))
-for i in range(len(x_wgs)):
-    yx_px[i][0] = (x_wgs[i] - x_left) * pixels_per_x_wgs
-    yx_px[i][1] = (y_upper - y_wgs[i]) * pixels_per_y_wgs
+yx_px = np.zeros((len(x_epsg), 2))
+for i in range(len(x_epsg)):
+    yx_px[i][0] = (x_epsg[i] - x_left) * pixels_per_x_epsg
+    yx_px[i][1] = (y_upper - y_epsg[i]) * pixels_per_y_epsg
 
 yx_px = yx_px.astype(int)
 
@@ -72,8 +61,8 @@ area_mask = np.zeros(img.shape[:2], np.uint8)
 # cv2.imshow('area_mask_small', area_mask_small)
 # cv2.waitKey()
 
-# cv2.polylines(img, pts=[yx_px], isClosed=True, color=55)
-# cv2.imwrite("/tmp/img1.tiff", img)
+cv2.polylines(img, pts=[yx_px], isClosed=True, color=55)
+cv2.imwrite("/tmp/img1.tiff", img)
 
 
 
