960f0970591cf83e9e7daaf37fb9d67d54aae759
Przemyslaw Aszkowski <przemyslaw.aszkowski@gmail.com>
2021-10-02 22:28:06 +0200

    config updated

--- src/config.py
+++ src/config.py
@@ -8,13 +8,13 @@ DAMAGE_AREA_FILE_NAME = 'szkody_placowe.gpkg'
 SUBDIRECTORIES_TO_PROCESS = [
     "kukurydza_5_ha",
     "kukurydza_10_ha",
-    # "kukurydza_11_ha",
-    # "kukurydza_13_ha",
-    # "kukurydza_15_ha",
-    # "kukurydza_18_ha",
-    # "kukurydza_25_ha",
-    # "kukurydza_38_ha",
-    # "kukurydza_60_ha",
+    "kukurydza_11_ha",
+    "kukurydza_13_ha",
+    "kukurydza_15_ha",
+    "kukurydza_18_ha",
+    "kukurydza_25_ha",
+    "kukurydza_38_ha",
+    "kukurydza_60_ha",
 ]
 
 FIELD_BORDER_EROSION_SIZE_PIXELS = 35  # [in pixels] Erode the field border, because sometimes it goes too far
