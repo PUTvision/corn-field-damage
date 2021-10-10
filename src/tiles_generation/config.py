9302c76fb273569a7c57b21771fdaa2e04a457cc
Przemyslaw Aszkowski <przemyslaw.aszkowski@gmail.com>
2021-10-10 18:22:26 +0200

    Training with stride 768. Train, validate and test on random tiles from all files

--- src/tiles_generation/config.py
+++ src/tiles_generation/config.py
@@ -3,7 +3,7 @@ import platform
 hostname = platform.node()
 if hostname == 'przemek-PC':
     BASE_DATA_DIR_PATH = "/media/data/nextcloud/Wspoldzielone/PP/corn/raw"  # directory with data as on google drive
-    BASE_OUTPUT_DATA_DIR_PATH = "/media/data/local/corn/"  # base directory where to save the processed data
+    BASE_OUTPUT_DATA_DIR_PATH = "/media/data/local/corn/processed"  # base directory where to save the processed data
 elif hostname == 'przemek-notebook':
     BASE_DATA_DIR_PATH = "/media/przemek/data/corn_data/raw"  # directory with data as on google drive
     BASE_OUTPUT_DATA_DIR_PATH = "/media/przemek/data/corn_data/processed"  # base directory where to save the processed data
@@ -16,19 +16,19 @@ FIELD_AREA_FILE_NAME = 'obszar.gpkg'
 DAMAGE_AREA_FILE_NAME = 'szkody_placowe.gpkg'
 
 SUBDIRECTORIES_TO_PROCESS = [
-    # "kukurydza_5_ha",
+    "kukurydza_5_ha",
     "kukurydza_10_ha",
-    # "kukurydza_11_ha",
-    # "kukurydza_13_ha",
-    # "kukurydza_15_ha",
-    # "kukurydza_18_ha",
-    # "kukurydza_25_ha",
-    # "kukurydza_38_ha",
-    # "kukurydza_60_ha",
+    "kukurydza_11_ha",
+    "kukurydza_13_ha",
+    "kukurydza_15_ha",
+    "kukurydza_18_ha",
+    "kukurydza_25_ha",
+    "kukurydza_38_ha",
+    "kukurydza_60_ha",
 ]
 
 FIELD_BORDER_EROSION_SIZE_PIXELS = 35  # [in pixels] Erode the field border, because sometimes it goes too far
-TILE_STRIDE = 256  # in pixels
+TILE_STRIDE = 768  # in pixels
 TILE_SIZE = (512 + 256)  # in pixels
 MINIMUM_TILE_FIELD_COVERAGE_PERCENTAGE = 75  # 0-100. Minimal percentage of corn field in the tile to save it
 
